6c9993e740096c618344ac570bb1f6b569445345
Mike Almond <mikedotalmond@googlemail.com>
2012-11-21 21:57:07 +0000

    Added some missing/new properties and updated starling.swf to a build from the latest starling commit - 4fcd52f2342512b793f9c426f5d854d283e254b3 https://github.com/PrimaryFeather/Starling-Framework
    Fixed a few little syntax errors.

--- starling/core/RenderSupport.hx
+++ starling/core/RenderSupport.hx
@@ -3,6 +3,7 @@ package starling.core;
 
 import flash.geom.Matrix;
 import flash.geom.Matrix3D;
+import flash.geom.Rectangle;
 import starling.display.DisplayObject;
 import starling.display.Quad;
 import starling.textures.Texture;
@@ -27,6 +28,20 @@ extern class RenderSupport {
 	var mvpMatrix3D (default, null):Matrix3D;
 	
 	static function transformMatrixForObject (matrix:Matrix, object:DisplayObject):Void;
+	
+	/** Configures the back buffer on the current context3D. By using this method, Starling
+	 *  can store the size of the back buffer and utilize this information in other methods
+	 *  (e.g. the scissor rectangle property). */
+	function configureBackBuffer(width:Int, height:Int, antiAlias:Int,  enableDepthAndStencil:Bool):Void;
+        
+	// scissor rect
+	
+	/** The scissor rectangle can be used to limit rendering in the current render target to
+	 *  a certain area. This method expects the rectangle in stage coordinates
+	 *  (different to the context3D method with the same name, which expects pixels).
+	 *  Pass <code>null</code> to turn off scissoring.
+	 *  CAUTION: not a copy -- use with care! */ 
+	var scissorRectangle(default, default):Rectangle;
 	
 	function pushBlendMode ():Void;
 	function popBlendMode ():Void;
